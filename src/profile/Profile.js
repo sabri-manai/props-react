205bbe7f796e4638ba232f57a73d961a37f5b8de
sabri-manai <sabri.manai@medtech.tn>
2021-07-09 15:47:14 +0100

    final commit

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import {Card, Button, Container} from 'react-bootstrap';
 const Profile = (props) => {
-    console.log(props.children)
+    // console.log(props.children)
 
     return (
         <div> 
-        <img style={{  verticalAlign:"middle", width: "200px", height: "200px", borderRadius: "50%"}} width="320" height="460" src={props.children} alt="profilePicture" />    
+        {props.children}   
         <h4>Full name:{props.fullName}</h4>
         <h5>Bio:{props.bio}</h5>
         <h5>Profession:{props.profession}</h5>
-<button onClick={()=>props.alertName()}>click to see my name</button>
-
-
+        <button onClick={()=>props.alertName(props.fullName)}>click to see my name</button>
         </div>
     )
-}
-
+    }
+    Profile.defaultProps = {
+        fullName: 'Mary'
+      };
 export default Profile
